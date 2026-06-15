9555a75487f5953288f5055c4b09f07bf8aa1b21
Rui Chen <rui@chenrui.dev>
2026-06-14 22:00:48 -0400

    bazel-remote: update homepage
    Signed-off-by: Rui Chen <rui@chenrui.dev>

--- Formula/b/bazel-remote.rb
+++ Formula/b/bazel-remote.rb
@@ -1,6 +1,6 @@
 class BazelRemote < Formula
   desc "Remote cache for Bazel"
-  homepage "https://github.com/buchgr/bazel-remote/"
+  homepage "https://bazel.build"
   url "https://github.com/buchgr/bazel-remote/archive/refs/tags/v2.6.1.tar.gz"
   sha256 "7e554caa4c9c7033459acc053ee8470208012223e72c81aabe2b48fb0e6c3fbc"
   license "Apache-2.0"
