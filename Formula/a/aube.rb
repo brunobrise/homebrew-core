87ed2a7c0386dcb6029765e953855ac7c9e0dd9c
Rui Chen <rui@chenrui.dev>
2026-06-14 21:19:55 -0400

    aube: update repo location and homepage
    Signed-off-by: Rui Chen <rui@chenrui.dev>

--- Formula/a/aube.rb
+++ Formula/a/aube.rb
@@ -1,7 +1,7 @@
 class Aube < Formula
   desc "Fast Node.js package manager"
-  homepage "https://aube.en.dev"
-  url "https://github.com/endevco/aube/archive/refs/tags/v1.21.0.tar.gz"
+  homepage "https://aube.jdx.dev/"
+  url "https://github.com/jdx/aube/archive/refs/tags/v1.21.0.tar.gz"
   sha256 "040823555a3301c2b719f808143525d4f98708a9a38c3f7f675ce5e9b725342f"
   license "MIT"
 
